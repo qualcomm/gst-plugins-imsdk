c8a4b58fa0391d0f1ec36e5cc3a0d0da56db8157
Petar G. Georgiev <quic_petarg@quicinc.com>
2022-03-10 08:45:15 +0200

    vsplit: Plugin for spliting video stream into multiple streams
    - Input video stream will be split into multiple streams equal
      to the number of source pads in a manner dependent on the mode.
    - In normal mode incoming buffer is rescaled and color converted
      for each of the source pads in order to match the negotiated caps.
    - In roi mode incoming bufferis checked for ROI meta. For each meta
      entry a crop, rescale and color conversion are performed, and then
      sent to the corresponding source pad. Pads with no corresponding ROI
      meta will produce GAP buffers.
    
    Change-Id: I4613e6c9754fe8c9a51d9ef258a5a1da2c691814

---
+++ gst-plugin-vsplit/videosplit.h
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the
+ * disclaimer below) provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *
+ *     * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+ * GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+ * HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef __GST_QTI_VIDEO_SPLIT_H__
+#define __GST_QTI_VIDEO_SPLIT_H__
+
+#include <gst/gst.h>
+#include <gst/video/video.h>
+
+#ifdef USE_GLES_CONVERTER
+#include <gst/video/gles-video-converter.h>
+#endif //USE_GLES_CONVERTER
+
+G_BEGIN_DECLS
+
+#define GST_TYPE_VIDEO_SPLIT (gst_video_split_get_type())
+#define GST_VIDEO_SPLIT(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST((obj),GST_TYPE_VIDEO_SPLIT,GstVideoSplit))
+#define GST_VIDEO_SPLIT_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_CAST((klass),GST_TYPE_VIDEO_SPLIT,GstVideoSplitClass))
+#define GST_IS_VIDEO_SPLIT(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE((obj),GST_TYPE_VIDEO_SPLIT))
+#define GST_IS_VIDEO_SPLIT_CLASS(obj) \
+  (G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_VIDEO_SPLIT))
+
+#define GST_VIDEO_SPLIT_GET_LOCK(obj) (&GST_VIDEO_SPLIT(obj)->lock)
+#define GST_VIDEO_SPLIT_LOCK(obj) \
+  g_mutex_lock(GST_VIDEO_SPLIT_GET_LOCK(obj))
+#define GST_VIDEO_SPLIT_UNLOCK(obj) \
+  g_mutex_unlock(GST_VIDEO_SPLIT_GET_LOCK(obj))
+
+typedef enum {
+  GST_VIDEO_SPLIT_MODE_NORMAL,
+  GST_VIDEO_SPLIT_MODE_ROI,
+} GstVideoSplitMode;
+
+typedef struct _GstVideoSplit GstVideoSplit;
+typedef struct _GstVideoSplitClass GstVideoSplitClass;
+
+struct _GstVideoSplit
+{
+  /// Inherited parent structure.
+  GstElement           parent;
+
+  /// Global mutex lock.
+  GMutex               lock;
+
+  /// Next available index for the source pads.
+  guint                nextidx;
+
+  /// Convenient local reference to sink pad.
+  GstPad               *sinkpad;
+  /// Convenient local reference to source pads.
+  GList                *srcpads;
+
+  /// Supported converters.
+#ifdef USE_GLES_CONVERTER
+  GstGlesConverter     *glesconvert;
+#endif // USE_GLES_CONVERTER
+
+  /// Properties.
+  GstVideoSplitMode    mode;
+};
+
+struct _GstVideoSplitClass {
+  /// Inherited parent structure.
+  GstElementClass parent;
+};
+
+GType gst_video_split_get_type (void);
+
+G_END_DECLS
+
+#endif // __GST_QTI_VIDEO_SPLIT_H__
