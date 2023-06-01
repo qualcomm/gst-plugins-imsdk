ec5086e1d2bb9de7b13a51ee81b259354596b8f7
ppachang <quic_ppachang@quicinc.com>
2023-06-01 16:40:46 +0530

    drmdecryptor: Playready DRM Decryptor Plugin
    Plugin to decrypt protected content using Playready DRM APIs.
    
    Change-Id: I42fc39568e83ac2075698f16e93b91b57a5d826f

---
+++ gst-plugin-drmdecryptor/drmdecryptor-engine.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
+#ifndef __GST_DRM_DECRYPTOR_ENGINE_H__
+#define __GST_DRM_DECRYPTOR_ENGINE_H__
+
+#include <gst/gst.h>
+#include <gst/video/video.h>
+
+G_BEGIN_DECLS
+
+#define GST_DRM_DECRYPTOR_ENGINE(obj)    ((GstDrmDecryptorEngine*)(obj))
+
+typedef struct _GstDrmDecryptorEngine GstDrmDecryptorEngine;
+
+GST_API GstDrmDecryptorEngine *
+gst_drm_decryptor_engine_new (const gchar *session_id);
+
+GST_API void
+gst_drm_decryptor_engine_free (GstDrmDecryptorEngine *engine);
+
+GST_API gboolean
+gst_drm_decryptor_engine_execute (GstDrmDecryptorEngine *engine,
+    GstBuffer *in_buffer, GstBuffer **out_buffer);
+
+G_END_DECLS
+
+#endif // __GST_DRM_DECRYPTOR_ENGINE_H__
