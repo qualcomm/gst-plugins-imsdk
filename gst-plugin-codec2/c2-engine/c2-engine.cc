4aef6557a41c33fcbef87dcc7a7f8830fa6753f5
ppachang <quic_ppachang@quicinc.com>
2023-07-17 23:45:58 +0530

    codec2: Fix SIGSEGV fault on drain with EOS
     Issue: Segmentation fault occurs on EOS when drain is called
            during stop decoder
    
     Fix: Wait for EOS buffer from codec2 backend before stopping the
          component and releasing the resources
    
    Note: Drain API is not returning an EOS buffer from video backend.
    Reverting back to queuing an empty buffer with EOS flag on drain
    until the issue is fixed by video team.
    
    Change-Id: I691302a67f8db625cf6de866c2e8caf63aeb4adf

--- gst-plugin-codec2/c2-engine/c2-engine.cc
+++ gst-plugin-codec2/c2-engine/c2-engine.cc
@@ -427,15 +427,31 @@ gboolean
 gst_c2_engine_drain (GstC2Engine * engine, gboolean eos)
 {
   C2Module *c2module = engine->c2module;
+  std::shared_ptr<C2Buffer> c2buffer;
+  std::list<std::unique_ptr<C2Param>> settings;
+
+  uint64_t index = 0;
+  uint64_t timestamp = 0;
+  uint32_t flags = C2FrameData::FLAG_END_OF_STREAM;
+
+  // TODO: Switch to Drain API when drain with EOS is supported.
+  // try {
+  //   c2module->Drain (eos ? C2Component::DRAIN_COMPONENT_WITH_EOS :
+  //       C2Component::DRAIN_COMPONENT_NO_EOS);
+  //   GST_DEBUG ("Drain c2module '%s'", engine->name);
+  // } catch (std::exception& e) {
+  //   GST_ERROR ("Failed to drain c2module, error: '%s'!", e.what());
+  //   return FALSE;
+  // }
 
   try {
-    c2module->Drain (eos ? C2Component::DRAIN_COMPONENT_WITH_EOS :
-        C2Component::DRAIN_COMPONENT_NO_EOS);
-    GST_DEBUG ("Drain c2module '%s'", engine->name);
+    c2module->Queue (c2buffer, settings, index, timestamp, flags);
   } catch (std::exception& e) {
-    GST_ERROR ("Failed to drain c2module, error: '%s'!", e.what());
+    GST_ERROR ("Failed to queue EOS, error: '%s'!", e.what());
     return FALSE;
   }
+
+  GST_C2_ENGINE_INCREMENT_PENDING_WORK (engine);
 
   // Wait until all work is completed or EOS.
   GST_C2_ENGINE_CHECK_AND_WAIT_PENDING_WORK (engine, 0);
