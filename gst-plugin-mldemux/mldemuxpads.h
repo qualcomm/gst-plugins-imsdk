4ffa260e7f9c9c74e8901849626ce9d4aca1cf8c
Petar G. Georgiev <quic_petarg@quicinc.com>
2023-11-17 19:02:50 +0200

    mldemux: Fix handling of EOS event
    Issue: EOS event was handled by just flushing internal queues from
           any buffers instead of processing them.
    Fix: Process all remaining buffers in the internal queues before
         forwarding EOS event downstream.
    
    Change-Id: I1a70c3dadf7a163765b50f76933e2a7e204acd0d

--- gst-plugin-mldemux/mldemuxpads.h
+++ gst-plugin-mldemux/mldemuxpads.h
@@ -63,6 +63,35 @@ G_BEGIN_DECLS
   (G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_ML_DEMUX_SRCPAD))
 #define GST_ML_DEMUX_SRCPAD_CAST(obj) ((GstMLDemuxSrcPad *)(obj))
 
+#define GST_ML_DEMUX_PAD_SIGNAL_IDLE(pad, idle) \
+{\
+  g_mutex_lock (&(pad->lock));                                     \
+                                                                   \
+  if (pad->is_idle != idle) {                                      \
+    pad->is_idle = idle;                                           \
+    GST_TRACE_OBJECT (pad, "State %s", idle ? "Idle" : "Running"); \
+    g_cond_signal (&(pad->drained));                               \
+  }                                                                \
+                                                                   \
+  g_mutex_unlock (&(pad->lock));                                   \
+}
+
+#define GST_ML_DEMUX_PAD_WAIT_IDLE(pad) \
+{\
+  g_mutex_lock (&(pad->lock));                                         \
+  GST_TRACE_OBJECT (pad, "Waiting until idle");                        \
+                                                                       \
+  while (!pad->is_idle) {                                              \
+    gint64 endtime = g_get_monotonic_time () + 1 * G_TIME_SPAN_SECOND; \
+                                                                       \
+    if (!g_cond_wait_until (&(pad->drained), &(pad->lock), endtime))   \
+      GST_WARNING_OBJECT (pad, "Timeout while waiting for idle!");     \
+  }                                                                    \
+                                                                       \
+  GST_TRACE_OBJECT (pad, "Received idle");                             \
+  g_mutex_unlock (&(pad->lock));                                       \
+}
+
 typedef struct _GstMLDemuxSinkPad GstMLDemuxSinkPad;
 typedef struct _GstMLDemuxSinkPadClass GstMLDemuxSinkPadClass;
 typedef struct _GstMLDemuxSrcPad GstMLDemuxSrcPad;
@@ -84,6 +113,14 @@ struct _GstMLDemuxSinkPadClass {
 struct _GstMLDemuxSrcPad {
   /// Inherited parent structure.
   GstPad       parent;
+
+  /// Global mutex lock.
+  GMutex       lock;
+
+  /// Condition for signalling that last buffer was submitted downstream.
+  GCond        drained;
+  /// Flag indicating that there is no more work for processing.
+  gboolean     is_idle;
 
   /// ML tensors info from caps.
   GstMLInfo    *mlinfo;
