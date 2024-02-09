2e2ac7834662780a96a3d6327c2625ea94cc890e
Petar G. Georgiev <quic_petarg@quicinc.com>
2024-02-09 11:55:54 +0200

    mlvdetection: Move prediction generation into separate function
    Move the processing on the input ML buffer into a separate function,
    which is executed before prepare_output_buffer(), so that we can send
    a GAP buffer when there are no predictions results. This will avoid
    the unnecessary use of a buffer with empty allocated memory.
    
    Change-Id: Id1cce4d32b67ef20f9b4ec25e035e37179e65cdd

--- gst-plugin-mlvdetection/mlvdetection.h
+++ gst-plugin-mlvdetection/mlvdetection.h
@@ -102,6 +102,8 @@ struct _GstMLVideoDetection {
 
   /// Tensor deciphering module.
   GstMLModule       *module;
+  /// Array with predictions from the module post-processing.
+  GArray            *predictions;
 
   /// Cairo surfaces and contexts mapped for each buffer.
   GHashTable        *surfaces;
