08bb4c95f9ebab1f307df095c67cfefe6bca36ce
Konstantin Motov <quic_kmotov@quicinc.com>
2024-06-12 15:50:29 +0300

    base, mlvconverter: fix sigma and scale units
    Issue: non-scale value is 1/255 in case of integer output but
    but non-scale value iw 1.0 in case of float output.
    
    Analysis: The issue is OpenGL pipeline is different for
    integer and float outputs. In case of float pipeline
    no-scave value is 255, but in case of interer output
    OpenGL pipeline scale up from 0.0-1.0 to 0-255 range
    automatically. Because of that non-scale value is 1.0.
    
    Fix: Update calculatoin so that non-scale value is always 1.0
    
    Change-Id: Ia682b38083e4c53e615c3bea499b7ef0beb003f3

--- gst-plugin-mlvconverter/mlvconverter.c
+++ gst-plugin-mlvconverter/mlvconverter.c
@@ -96,7 +96,9 @@ G_DEFINE_TYPE (GstMLVideoConverter, gst_ml_video_converter,
 #define DEFAULT_PROP_ENGINE_BACKEND  (gst_video_converter_default_backend())
 #define DEFAULT_PROP_SUBPIXEL_LAYOUT GST_ML_VIDEO_PIXEL_LAYOUT_REGULAR
 #define DEFAULT_PROP_MEAN            0.0
-#define DEFAULT_PROP_SIGMA           (1.0 / 255.0)
+#define DEFAULT_PROP_SIGMA           1.0
+
+#define FLOAT_CONVERSION_SIGMA       (255.0)
 
 #define GINT8_PTR_CAST(data)         ((gint8*) data)
 #define GUINT8_PTR_CAST(data)        ((guint8*) data)
@@ -552,6 +554,11 @@ gst_ml_video_converter_normalize_ip (GstMLVideoConverter * mlconverter,
   for (idx = 0; idx < bpp; idx++) {
     mean[idx] = GET_MEAN_VALUE (mlconverter->mean, idx);
     sigma[idx] = GET_SIGMA_VALUE (mlconverter->sigma, idx);
+
+    // Apply coefficients for float conversion.
+    if (GST_ML_INFO_TYPE (mlconverter->mlinfo) == GST_ML_TYPE_FLOAT16 ||
+        GST_ML_INFO_TYPE (mlconverter->mlinfo) == GST_ML_TYPE_FLOAT32)
+      sigma[idx] /= FLOAT_CONVERSION_SIGMA;
   }
 
   source = GST_VIDEO_FRAME_PLANE_DATA (vframe, 0);
@@ -606,6 +613,11 @@ gst_ml_video_converter_normalize (GstMLVideoConverter * mlconverter,
   for (idx = 0; idx < bpp; idx++) {
     mean[idx] = GET_MEAN_VALUE (mlconverter->mean, idx);
     sigma[idx] = GET_SIGMA_VALUE (mlconverter->sigma, idx);
+
+    // Apply coefficients for float conversion.
+    if (GST_ML_INFO_TYPE (mlconverter->mlinfo) == GST_ML_TYPE_FLOAT16 ||
+        GST_ML_INFO_TYPE (mlconverter->mlinfo) == GST_ML_TYPE_FLOAT32)
+      sigma[idx] /= FLOAT_CONVERSION_SIGMA;
   }
 
   source = GST_VIDEO_FRAME_PLANE_DATA (inframe, 0);
