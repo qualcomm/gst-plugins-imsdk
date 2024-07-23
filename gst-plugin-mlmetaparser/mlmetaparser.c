29a501f1daaf06a80a194a691019251b9431f4fe
Angel Hitov <quic_ahitov@quicinc.com>
2024-07-23 17:56:34 +0300

    qtimlmetaparser: Add ML metadata parser plugin.
    Receives ML metadata and parse to specific format (JSON)
    
    Change-Id: Ibe4bcb2d5f9fed3c3ca5c636e840603c67f4a104

---
+++ gst-plugin-mlmetaparser/mlmetaparser.c
@@ -0,0 +1,438 @@
+/*
+ * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "mlmetaparser.h"
+
+#include <gst/utils/common-utils.h>
+#include <gst/video/gstvideoclassificationmeta.h>
+#include <gst/video/gstvideolandmarksmeta.h>
+
+#define GST_TYPE_PARSER_MODULES (gst_ml_meta_parser_modules_get_type())
+
+#define GST_CAT_DEFAULT gst_ml_meta_parser_debug
+GST_DEBUG_CATEGORY_STATIC (gst_ml_meta_parser_debug);
+
+#define gst_ml_meta_parser_parent_class parent_class
+G_DEFINE_TYPE (GstMlMetaParser, gst_ml_meta_parser, GST_TYPE_BASE_TRANSFORM);
+
+#define DEFAULT_MIN_BUFFERS      2
+#define DEFAULT_MAX_BUFFERS      10
+#define DEFAULT_TEXT_BUFFER_SIZE 24576
+
+#define DEFAULT_PROP_MODULE 0
+
+#define GST_ML_META_PARSER_SINK_CAPS \
+    "video/x-raw(ANY); " \
+    "text/x-raw, format = (string) utf8"
+
+#define GST_ML_META_PARSER_SRC_CAPS \
+    "text/x-raw, format = (string) utf8"
+
+enum
+{
+  PROP_0,
+  PROP_MODULE
+};
+
+static GstStaticPadTemplate gst_ml_meta_parser_sink_template =
+    GST_STATIC_PAD_TEMPLATE("sink",
+        GST_PAD_SINK,
+        GST_PAD_ALWAYS,
+        GST_STATIC_CAPS (GST_ML_META_PARSER_SINK_CAPS)
+    );
+static GstStaticPadTemplate gst_ml_meta_parser_src_template =
+    GST_STATIC_PAD_TEMPLATE("src",
+        GST_PAD_SRC,
+        GST_PAD_ALWAYS,
+        GST_STATIC_CAPS (GST_ML_META_PARSER_SRC_CAPS)
+    );
+
+static GType
+gst_ml_meta_parser_modules_get_type (void)
+{
+  static GType gtype = 0;
+  static GEnumValue *variants = NULL;
+
+  if (gtype)
+    return gtype;
+
+  variants = gst_ml_enumarate_modules ("ml-meta-parser-");
+  gtype = g_enum_register_static ("GstMLParserModules", variants);
+
+  return gtype;
+}
+
+static GstBufferPool *
+gst_ml_meta_parser_create_pool (GstMlMetaParser * mlmetaparser, GstCaps * caps)
+{
+  GstBufferPool *pool = NULL;
+  GstStructure *structure = NULL;
+  guint size = 0;
+
+  GST_INFO_OBJECT (mlmetaparser, "Uses SYSTEM memory");
+
+  if ((pool = gst_buffer_pool_new ()) == NULL) {
+    GST_ERROR_OBJECT (mlmetaparser, "Failed to create buffer pool!");
+    return NULL;
+  }
+
+  size = DEFAULT_TEXT_BUFFER_SIZE;
+
+  structure = gst_buffer_pool_get_config (pool);
+  gst_buffer_pool_config_set_params (structure, caps, size,
+      DEFAULT_MIN_BUFFERS, DEFAULT_MAX_BUFFERS);
+
+  if (!gst_buffer_pool_set_config (pool, structure)) {
+    GST_WARNING_OBJECT (mlmetaparser, "Failed to set pool configuration!");
+    g_object_unref (pool);
+    pool = NULL;
+  }
+
+  return pool;
+}
+
+static gboolean
+gst_ml_meta_parser_decide_allocation (GstBaseTransform * base, GstQuery * query)
+{
+  GstMlMetaParser *mlmetaparser = GST_ML_META_PARSER (base);
+  GstCaps *caps = NULL;
+  GstBufferPool *pool = NULL;
+  GstStructure *config = NULL;
+  GstAllocator *allocator = NULL;
+  guint size, minbuffers, maxbuffers;
+  GstAllocationParams params;
+
+  gst_query_parse_allocation (query, &caps, NULL);
+  if (!caps) {
+    GST_ERROR_OBJECT (mlmetaparser, "Failed to parse the allocation caps!");
+    return FALSE;
+  }
+
+  // Invalidate the cached pool if there is an allocation_query.
+  if (mlmetaparser->outpool)
+    g_clear_pointer (&(mlmetaparser->outpool), gst_object_unref);
+
+  // Create a new buffer pool.
+  if ((pool = gst_ml_meta_parser_create_pool (mlmetaparser, caps)) == NULL) {
+    GST_ERROR_OBJECT (mlmetaparser, "Failed to create buffer pool!");
+    return FALSE;
+  }
+
+  mlmetaparser->outpool = pool;
+
+  // Get the configured pool properties in order to set in query.
+  config = gst_buffer_pool_get_config (pool);
+  gst_buffer_pool_config_get_params (config, &caps, &size, &minbuffers,
+      &maxbuffers);
+
+  if (gst_buffer_pool_config_get_allocator (config, &allocator, &params))
+    gst_query_add_allocation_param (query, allocator, &params);
+
+  gst_structure_free (config);
+
+  // Check whether the query has pool.
+  if (gst_query_get_n_allocation_pools (query) > 0)
+    gst_query_set_nth_allocation_pool (query, 0, pool, size, minbuffers,
+        maxbuffers);
+  else
+    gst_query_add_allocation_pool (query, pool, size, minbuffers,
+        maxbuffers);
+
+  return TRUE;
+}
+
+static GstFlowReturn
+gst_ml_meta_parser_prepare_output_buffer (GstBaseTransform * base,
+    GstBuffer * inbuffer, GstBuffer ** outbuffer)
+{
+  GstMlMetaParser *mlmetaparser = GST_ML_META_PARSER (base);
+  GstBufferPool *pool = mlmetaparser->outpool;
+
+  if (gst_base_transform_is_passthrough (base)) {
+    GST_DEBUG_OBJECT (mlmetaparser, "Passthrough, no need to do anything");
+    *outbuffer = inbuffer;
+    return GST_FLOW_OK;
+  }
+
+  if (!gst_buffer_pool_is_active (pool) &&
+      !gst_buffer_pool_set_active (pool, TRUE)) {
+    GST_ERROR_OBJECT (mlmetaparser, "Failed to activate output buffer pool!");
+    return GST_FLOW_ERROR;
+  }
+
+  // Input is marked as GAP, nothing to process. Create a GAP output buffer.
+  if (gst_buffer_get_size (inbuffer) == 0 &&
+      GST_BUFFER_FLAG_IS_SET (inbuffer, GST_BUFFER_FLAG_GAP))
+    *outbuffer = gst_buffer_new ();
+
+  if ((*outbuffer == NULL) &&
+      gst_buffer_pool_acquire_buffer (pool, outbuffer, NULL) != GST_FLOW_OK) {
+    GST_ERROR_OBJECT (mlmetaparser, "Failed to create output buffer!");
+    return GST_FLOW_ERROR;
+  }
+
+  // Copy the timestamps from the input buffer.
+  gst_buffer_copy_into (*outbuffer, inbuffer, GST_BUFFER_COPY_TIMESTAMPS, 0, -1);
+
+  return GST_FLOW_OK;
+}
+
+static GstCaps *
+gst_ml_meta_parser_transform_caps (GstBaseTransform * base,
+    GstPadDirection direction, GstCaps * caps, GstCaps * filter)
+{
+  GstMlMetaParser *mlmetaparser = GST_ML_META_PARSER (base);
+  GstCaps *result = NULL;
+
+  GST_DEBUG_OBJECT (mlmetaparser, "Transforming caps: %" GST_PTR_FORMAT
+      " in direction %s", caps, (direction == GST_PAD_SINK) ? "sink" : "src");
+  GST_DEBUG_OBJECT (mlmetaparser, "Filter caps: %" GST_PTR_FORMAT, filter);
+
+  // The source and sink pads caps do not depend on each other so directly
+  // take the template caps for the corresponding pad and apply filter.
+  if (direction == GST_PAD_SINK)
+    result = gst_pad_get_pad_template_caps (GST_BASE_TRANSFORM_SRC_PAD (base));
+  else if (direction == GST_PAD_SRC)
+    result = gst_pad_get_pad_template_caps (GST_BASE_TRANSFORM_SINK_PAD (base));
+
+  GST_DEBUG_OBJECT (mlmetaparser, "Template caps: %" GST_PTR_FORMAT, result);
+
+  if (filter) {
+    GstCaps *intersection =
+        gst_caps_intersect_full (filter, result, GST_CAPS_INTERSECT_FIRST);
+    gst_caps_unref (result);
+    result = intersection;
+  }
+
+  GST_DEBUG_OBJECT (mlmetaparser, "Returning caps: %" GST_PTR_FORMAT, result);
+  return result;
+}
+
+static gboolean
+gst_ml_meta_parser_accept_caps (GstBaseTransform * base,
+    GstPadDirection direction, GstCaps * caps)
+{
+  GstMlMetaParser *mlmetaparser = GST_ML_META_PARSER (base);
+  GstCaps *tmplcaps = NULL;
+  gboolean success = FALSE;
+
+  GST_DEBUG_OBJECT (mlmetaparser, "Accept caps: %" GST_PTR_FORMAT
+      " in direction %s", caps, (direction == GST_PAD_SINK) ? "sink" : "src");
+
+  if (direction == GST_PAD_SINK)
+    tmplcaps = gst_pad_get_pad_template_caps (
+        GST_BASE_TRANSFORM_SINK_PAD (base));
+  else if (direction == GST_PAD_SRC)
+    tmplcaps = gst_pad_get_pad_template_caps (
+        GST_BASE_TRANSFORM_SRC_PAD (base));
+
+  GST_DEBUG_OBJECT (mlmetaparser, "Template caps: %" GST_PTR_FORMAT, tmplcaps);
+
+  success = gst_caps_can_intersect (caps, tmplcaps);
+  gst_caps_unref (tmplcaps);
+
+  if (!success)
+    GST_WARNING_OBJECT (base, "Caps can't intersect!");
+
+  return success;
+}
+
+static gboolean
+gst_ml_meta_parser_set_caps (GstBaseTransform * base, GstCaps * incaps,
+    GstCaps * outcaps)
+{
+  GstMlMetaParser *mlmetaparser = GST_ML_META_PARSER (base);
+  GEnumClass *eclass = NULL;
+  GEnumValue *evalue = NULL;
+  GstStructure *structure;
+
+  // TODO Could be used for some initialization of a core component.
+  // If not used should be removed.
+  GST_DEBUG_OBJECT (mlmetaparser, "Output caps: %" GST_PTR_FORMAT, outcaps);
+
+  eclass = G_ENUM_CLASS (g_type_class_peek (GST_TYPE_PARSER_MODULES));
+  evalue = g_enum_get_value (eclass, mlmetaparser->mdlenum);
+
+  gst_parser_module_free (mlmetaparser->module);
+  mlmetaparser->module = gst_parser_module_new (evalue->value_name);
+
+  if (!gst_parser_module_init (mlmetaparser->module)) {
+    GST_ELEMENT_ERROR (mlmetaparser, RESOURCE, FAILED, (NULL),
+        ("Module initialization failed!"));
+    return FALSE;
+  }
+
+  structure = gst_structure_new ("options",
+      GST_PARSER_MODULE_OPT_CAPS, GST_TYPE_CAPS, incaps,
+      NULL);
+
+  if (!gst_parser_module_set_opts (mlmetaparser->module, structure)) {
+    GST_ELEMENT_ERROR (mlmetaparser, RESOURCE, FAILED, (NULL),
+        ("Failed to set module options!"));
+    return FALSE;
+  }
+
+  return TRUE;
+}
+
+static GstFlowReturn
+gst_ml_meta_parser_transform (GstBaseTransform * base, GstBuffer * inbuffer,
+    GstBuffer * outbuffer)
+{
+  GstMlMetaParser *mlmetaparser = GST_ML_META_PARSER (base);
+  gchar *output_string = NULL;
+  gsize output_size = 0;
+  GstMapInfo outmap = { 0, };
+  GstClockTime time = GST_CLOCK_TIME_NONE;
+
+  // GAP buffer, nothing to do. Propagate output buffer downstream.
+  if (gst_buffer_get_size (outbuffer) == 0 &&
+      GST_BUFFER_FLAG_IS_SET (outbuffer, GST_BUFFER_FLAG_GAP))
+    return GST_FLOW_OK;
+
+  if (!gst_buffer_map (outbuffer, &outmap, GST_MAP_READWRITE)) {
+    GST_ERROR ("Unable to map buffer!");
+    return GST_FLOW_ERROR;
+  }
+
+  time = gst_util_get_timestamp ();
+
+  gst_parser_module_execute (mlmetaparser->module, inbuffer, &output_string);
+
+  if (output_string) {
+    output_size = strlen (output_string) + 1;
+    g_strlcpy ((gchar *) outmap.data, output_string, output_size);
+    GST_LOG_OBJECT (mlmetaparser, "module output: %s", output_string);
+    g_free (output_string);
+  }
+
+  gst_buffer_unmap (outbuffer, &outmap);
+  gst_buffer_resize (outbuffer, 0, output_size);
+
+  GST_LOG_OBJECT (mlmetaparser, "output buffer size %zu", output_size);
+
+  time = GST_CLOCK_DIFF (time, gst_util_get_timestamp ());
+
+  GST_LOG_OBJECT (mlmetaparser, "Execute took %" G_GINT64_FORMAT ".%03"
+      G_GINT64_FORMAT " ms", GST_TIME_AS_MSECONDS (time),
+      (GST_TIME_AS_USECONDS (time) % 1000));
+
+  return GST_FLOW_OK;
+}
+
+static void
+gst_ml_meta_parser_set_property (GObject * object, guint prop_id,
+    const GValue * value, GParamSpec * pspec)
+{
+  GstMlMetaParser *mlmetaparser = GST_ML_META_PARSER (object);
+
+  switch (prop_id) {
+    case PROP_MODULE:
+      mlmetaparser->mdlenum = g_value_get_enum (value);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+  }
+}
+
+static void
+gst_ml_meta_parser_get_property (GObject * object, guint prop_id,
+    GValue * value, GParamSpec * pspec)
+{
+  GstMlMetaParser *mlmetaparser = GST_ML_META_PARSER (object);
+
+  switch (prop_id) {
+    case PROP_MODULE:
+      g_value_set_enum (value, mlmetaparser->mdlenum);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+  }
+}
+
+static void
+gst_ml_meta_parser_finalize (GObject * object)
+{
+  GstMlMetaParser *mlmetaparser = GST_ML_META_PARSER (object);
+
+  if (mlmetaparser->outpool != NULL)
+    gst_object_unref (mlmetaparser->outpool);
+
+  G_OBJECT_CLASS (parent_class)->finalize (G_OBJECT (mlmetaparser));
+}
+
+static void
+gst_ml_meta_parser_class_init (GstMlMetaParserClass * klass)
+{
+  GObjectClass *gobject       = G_OBJECT_CLASS (klass);
+  GstElementClass *element    = GST_ELEMENT_CLASS (klass);
+  GstBaseTransformClass *base = GST_BASE_TRANSFORM_CLASS (klass);
+
+  gobject->set_property = GST_DEBUG_FUNCPTR (gst_ml_meta_parser_set_property);
+  gobject->get_property = GST_DEBUG_FUNCPTR (gst_ml_meta_parser_get_property);
+  gobject->finalize     = GST_DEBUG_FUNCPTR (gst_ml_meta_parser_finalize);
+
+  g_object_class_install_property (gobject, PROP_MODULE,
+      g_param_spec_enum ("module", "Module",
+          "Module name that is going to be used for parsing metadata",
+          GST_TYPE_PARSER_MODULES, DEFAULT_PROP_MODULE,
+          G_PARAM_CONSTRUCT | G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
+
+  gst_element_class_set_static_metadata (element,
+      "Meta parser", "Filter/Effect/Converter", "Mate parsing plugin", "QTI");
+
+  gst_element_class_add_pad_template (element,
+      gst_static_pad_template_get (&gst_ml_meta_parser_sink_template));
+  gst_element_class_add_pad_template (element,
+      gst_static_pad_template_get (&gst_ml_meta_parser_src_template));
+
+  base->decide_allocation =
+      GST_DEBUG_FUNCPTR (gst_ml_meta_parser_decide_allocation);
+  base->prepare_output_buffer =
+      GST_DEBUG_FUNCPTR (gst_ml_meta_parser_prepare_output_buffer);
+
+  base->transform_caps = GST_DEBUG_FUNCPTR (gst_ml_meta_parser_transform_caps);
+  base->accept_caps = GST_DEBUG_FUNCPTR (gst_ml_meta_parser_accept_caps);
+  base->set_caps = GST_DEBUG_FUNCPTR (gst_ml_meta_parser_set_caps);
+
+  base->transform = GST_DEBUG_FUNCPTR (gst_ml_meta_parser_transform);
+}
+
+static void
+gst_ml_meta_parser_init (GstMlMetaParser * mlmetaparser)
+{
+  mlmetaparser->outpool = NULL;
+
+  // Handle buffers with GAP flag internally.
+  gst_base_transform_set_gap_aware (GST_BASE_TRANSFORM (mlmetaparser), TRUE);
+
+  GST_DEBUG_CATEGORY_INIT (gst_ml_meta_parser_debug, "qtimlmetaparser", 0,
+      "QTI ML meta parser plugin");
+}
+
+static gboolean
+plugin_init (GstPlugin * plugin)
+{
+  return gst_element_register (plugin, "qtimlmetaparser", GST_RANK_NONE,
+      GST_TYPE_ML_META_PARSER);
+}
+
+GST_PLUGIN_DEFINE (
+    GST_VERSION_MAJOR,
+    GST_VERSION_MINOR,
+    qtimlmetaparser,
+    "QTI ML meta parsing plugin",
+    plugin_init,
+    PACKAGE_VERSION,
+    PACKAGE_LICENSE,
+    PACKAGE_SUMMARY,
+    PACKAGE_ORIGIN
+)
---
+++ gst-plugin-mlmetaparser/parsermodule.c
@@ -0,0 +1,228 @@
+/*
+ * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
+
+#include "parsermodule.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+#include <unistd.h>
+
+
+#define GST_CAT_DEFAULT gst_parser_module_debug
+GST_DEBUG_CATEGORY (gst_parser_module_debug);
+
+#define GST_PARSER_MODULE_OPEN_FUNC      "gst_parser_module_open"
+#define GST_PARSER_MODULE_CLOSE_FUNC     "gst_parser_module_close"
+#define GST_PARSER_MODULE_CONFIGURE_FUNC "gst_parser_module_configure"
+#define GST_PARSER_MODULE_PROCESS_FUNC   "gst_parser_module_process"
+
+/**
+ * _GstParserModule:
+ * @handle: Library handle.
+ * @name: Library (Module) name.
+ * @submodule: Pointer to private module structure.
+ *
+ * @open: Function pointer to the submodule 'gst_parser_module_open' API.
+ * @close: Function pointer to the submodule 'gst_parser_module_close' API.
+ * @configure: Function pointer to the submodule 'gst_parser_module_configure' API.
+ * @process: Function pointer to the submodule 'gst_parser_module_process' API.
+ *
+ * Machine learning interface for post-processing module.
+ */
+struct _GstParserModule {
+  gpointer             handle;
+  gchar                *name;
+  gpointer             submodule;
+
+  /// Interface functions.
+  GstParserModuleOpen      open;
+  GstParserModuleClose     close;
+  GstParserModuleConfigure configure;
+  GstParserModuleProcess   process;
+};
+
+static inline void
+gst_parser_module_initialize_debug_category (void)
+{
+  static gsize catonce = 0;
+
+  if (g_once_init_enter (&catonce)) {
+    GST_DEBUG_CATEGORY_INIT (gst_parser_module_debug,
+        "mlmetaparsermodule", 0, "QTI ML meta parser module");
+    g_once_init_leave (&catonce, TRUE);
+  }
+}
+
+static gboolean
+gst_parser_module_symbol (gpointer handle, const gchar * name, gpointer * symbol)
+{
+  *(symbol) = dlsym (handle, name);
+  if (NULL == *(symbol)) {
+    GST_ERROR ("Failed to link library method %s, error: %s!", name, dlerror());
+    return FALSE;
+  }
+  return TRUE;
+}
+
+GstParserModule *
+gst_parser_module_new (const gchar * name)
+{
+  GstParserModule *module = NULL;
+  gchar *location = NULL;
+  gboolean success = TRUE;
+
+  // Initialize the debug category.
+  gst_parser_module_initialize_debug_category ();
+
+  module = g_new0 (GstParserModule, 1);
+  location = g_strdup_printf ("%s/lib%s.so", GST_PARSER_MODULES_DIR, name);
+
+  module->name = g_strdup (name);
+  module->handle = dlopen (location, RTLD_NOW);
+
+  g_free (location);
+
+  if (module->handle == NULL) {
+    GST_ERROR ("Failed to open %s library, error: %s!", module->name, dlerror());
+    gst_parser_module_free (module);
+    return NULL;
+  }
+
+  success &= gst_parser_module_symbol (module->handle,
+      GST_PARSER_MODULE_OPEN_FUNC, (gpointer) &(module)->open);
+  success &= gst_parser_module_symbol (module->handle,
+      GST_PARSER_MODULE_CLOSE_FUNC, (gpointer) &(module)->close);
+  success &= gst_parser_module_symbol (module->handle,
+      GST_PARSER_MODULE_CONFIGURE_FUNC, (gpointer) &(module)->configure);
+  success &= gst_parser_module_symbol (module->handle,
+      GST_PARSER_MODULE_PROCESS_FUNC, (gpointer) &(module)->process);
+
+  if (!success) {
+    gst_parser_module_free (module);
+    return NULL;
+  }
+
+  GST_INFO ("Created %s module: %p", module->name, module);
+  return module;
+}
+
+void
+gst_parser_module_free (GstParserModule * module)
+{
+  if (NULL == module)
+    return;
+
+  if (module->submodule != NULL)
+    module->close (module->submodule);
+
+  if (module->handle != NULL)
+    dlclose (module->handle);
+
+  GST_INFO ("Destroyed %s module: %p", module->name, module);
+
+  if (module->name != NULL)
+    g_free (module->name);
+
+  g_free (module);
+  return;
+}
+
+gboolean
+gst_parser_module_init (GstParserModule * module)
+{
+  g_return_val_if_fail (module != NULL, FALSE);
+
+  if (module->submodule == NULL)
+    module->submodule = module->open ();
+
+  return (module->submodule != NULL) ? TRUE : FALSE;
+}
+
+gboolean
+gst_parser_module_set_opts (GstParserModule * module, GstStructure * options)
+{
+  g_return_val_if_fail (module != NULL, FALSE);
+  g_return_val_if_fail (options != NULL, FALSE);
+
+  return module->configure (module->submodule, options);
+}
+
+gboolean
+gst_parser_module_execute (GstParserModule * module, GstBuffer * inbuffer,
+    gchar ** output)
+{
+  g_return_val_if_fail (module != NULL, FALSE);
+  g_return_val_if_fail (inbuffer != NULL, FALSE);
+  g_return_val_if_fail (output != NULL, FALSE);
+
+  return module->process (module->submodule, inbuffer, output);
+}
+
+GEnumValue *
+gst_ml_enumarate_modules (const gchar * type)
+{
+  GEnumValue *variants = NULL;
+  GDir *directory = NULL;
+  const gchar *filename = NULL;
+  gchar *prefix = NULL, *string = NULL, *name = NULL, *shortname = NULL;
+  guint idx = 0, n_bytes = 0;
+
+  n_bytes = sizeof (GEnumValue);
+  variants = g_malloc (n_bytes * 2);
+
+  // Initialize the default value.
+  variants[idx].value = idx;
+  variants[idx].value_name = "No module, default invalid mode";
+  variants[idx].value_nick = "none";
+
+  idx++;
+
+  directory = g_dir_open (GST_PARSER_MODULES_DIR, 0, NULL);
+  prefix = g_strdup_printf ("lib%s", type);
+
+  while ((directory != NULL) && (filename = g_dir_read_name (directory))) {
+    gboolean isvalid = FALSE;
+
+    if (!g_str_has_prefix (filename, prefix))
+      continue;
+
+    if (!g_str_has_suffix (filename, ".so"))
+      continue;
+
+    string = g_strdup_printf ("%s/%s", GST_PARSER_MODULES_DIR, filename);
+    isvalid = !g_file_test (string, G_FILE_TEST_IS_DIR | G_FILE_TEST_IS_SYMLINK);
+    g_free (string);
+
+    if (!isvalid)
+      continue;
+
+    // Trim the 'lib' prefix and '.so' suffix.
+    name = g_strndup (filename + 3, strlen (filename) - 6);
+
+    // Extract only the unique module name.
+    shortname = g_utf8_strdown (name + strlen (type), -1);
+
+    variants = g_realloc (variants, n_bytes * (idx + 2));
+
+    variants[idx].value = idx;
+    variants[idx].value_name = name;
+    variants[idx].value_nick = shortname;
+
+    idx++;
+  }
+
+  // Last enum entry should be zero.
+  variants[idx].value = 0;
+  variants[idx].value_name = NULL;
+  variants[idx].value_nick = NULL;
+
+  g_free (prefix);
+
+  if (directory != NULL)
+    g_dir_close (directory);
+
+  return variants;
+}
