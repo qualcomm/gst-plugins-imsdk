b09f8937d7ceadcdf9064dad0219c1cf1c022eb1
Ishu Raj <quic_iraj@quicinc.com>
2024-09-09 11:56:51 +0530

    gst-python-examples: Add python gst sample apps
    Camera -> AI Detection (YoloV8) -> Overlay -> Display
    
    Change-Id: Idaca83e2ec673ef7e1171f8a62de9e93f3da1bd0

---
+++ gst-python-examples/gst-camera-detect-display.py
@@ -0,0 +1,221 @@
+# Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+
+import os
+import sys
+import signal
+import argparse
+
+import gi
+gi.require_version('Gst', '1.0')
+gi.require_version("GLib", "2.0")
+from gi.repository import Gst, GLib
+
+# Constants
+DESCRIPTION = """
+This app sets up GStreamer pipeline for object detection.
+Initializes and links elements for capturing, performs inference
+(object detection) using MODEL and LABELS files, and renders video
+on display.
+"""
+DEFAULT_MODEL_FILE = "/opt/data/YOLOv8-Detection-Quantized.tflite"
+DEFAULT_LABELS_FILE = "/opt/data/yolov8.labels"
+DELEGATE_PATH = "libQnnTFLiteDelegate.so"
+
+waiting_for_eos = False
+def handle_interrupt_signal(pipeline, mloop):
+    """Handle Ctrl+C."""
+    global waiting_for_eos
+
+    _, state, _ = pipeline.get_state(0)
+    if state != Gst.State.PLAYING or waiting_for_eos:
+        mloop.quit()
+        return GLib.SOURCE_CONTINUE
+
+    event = Gst.Event.new_eos()
+    if pipeline.send_event(event):
+        print("EoS sent to the pipeline")
+        waiting_for_eos = True
+    else:
+        print("Failed to send EoS event to the pipeline!")
+        mloop.quit()
+
+    return GLib.SOURCE_CONTINUE
+
+def handle_bus_message(bus, message, mloop):
+    """Handle messages posted on pipeline bus."""
+
+    if message.type == Gst.MessageType.ERROR:
+        error, debug_info = message.parse_error()
+        print("ERROR:", message.src.get_name(), " ", error.message)
+        if debug_info:
+            print("debugging info:", debug_info)
+        mloop.quit()
+    elif message.type == Gst.MessageType.EOS:
+        print("EoS received")
+        mloop.quit()
+
+    return True
+
+def create_element(factory_name, name):
+    """Create a GStreamer element."""
+    element = Gst.ElementFactory.make(factory_name, name)
+    if not element:
+        raise Exception(f"Unable to create element {name}")
+
+    return element
+
+
+def link_elements(elements):
+    """Link elements in the specified order."""
+    link_orders = [
+        [
+           "camsrc", "cam_caps", "queue0", "split"
+        ],
+        [
+            "split", "queue1", "metamux", "overlay", "display"
+        ],
+        [
+            "split", "converter", "queue2", "tflite", "queue3", "detection",
+            "tensor_caps", "queue4", "metamux"
+        ]
+    ]
+    for link_order in link_orders:
+        src = None  # Initialize src to None at the start of each link_order
+        for element in link_order:
+            dest = elements[element]
+            if src and not src.link(dest):
+                    raise Exception(
+                        f"Unable to link element "
+                        f"{src.get_name()} to {dest.get_name()}"
+                    )
+            src = dest  # Update src to the current dest for the next iteration
+
+def create_pipeline(pipeline):
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(
+        description=DESCRIPTION,
+        formatter_class=type(
+            'CustomFormatter',
+            (argparse.ArgumentDefaultsHelpFormatter, argparse.RawTextHelpFormatter),
+            {}
+        )
+    )
+    parser.add_argument(
+        "--model", type=str, default=DEFAULT_MODEL_FILE,
+        help="Model File Path"
+    )
+    parser.add_argument(
+        "--labels", type=str, default=DEFAULT_LABELS_FILE,
+        help="Labels File Path"
+    )
+    args = parser.parse_args()
+
+    # Create elements
+    elements = {
+        "camsrc"    : create_element("qtiqmmfsrc", "camsrc"),
+        "cam_caps"  : create_element("capsfilter", "cam_caps"),
+        "split"     : create_element("tee", "split"),
+        "metamux"   : create_element("qtimetamux", "metamux"),
+        "overlay"   : create_element("qtioverlay", "overlay"),
+        "display"   : create_element("waylandsink", "display"),
+        "converter" : create_element("qtimlvconverter", "converter"),
+        "tflite"    : create_element("qtimltflite", "tflite"),
+        "detection" : create_element("qtimlvdetection", "detection"),
+        "tensor_caps": create_element("capsfilter", "tensor_caps")
+    }
+
+    queue_count = 5
+    for i in range(queue_count):
+        queue_name = f"queue{i}"
+        elements[queue_name] = create_element("queue", queue_name)
+
+    # Set properties
+    elements["cam_caps"].set_property(
+        "caps", Gst.Caps.from_string(
+            "video/x-raw(memory:GBM),format=NV12,width=1280,height=720,"
+            "framerate=30/1,compression=ubwc"
+        )
+    )
+
+    elements["overlay"].set_property("engine", 2)
+
+    elements["tflite"].set_property("delegate", "external")
+    elements["tflite"].set_property("external-delegate-path", DELEGATE_PATH)
+    elements["tflite"].set_property("model", args.model)
+
+    elements["detection"].set_property("threshold", 75.0)
+    elements["detection"].set_property("results", 10)
+    elements["detection"].set_property("module", "yolov8")
+    elements["detection"].set_property("labels", args.labels)
+    elements["detection"].set_property(
+        "constants",
+        "YOLOv8,q-offsets=<-107.0, -128.0, 0.0>,"
+        "q-scales=<3.093529462814331, 0.00390625, 1.0>;"
+    )
+
+    elements["tensor_caps"].set_property("caps",
+        Gst.Caps.from_string("text/x-raw")
+    )
+    elements["display"].set_property("fullscreen", True)
+
+    # Create GstStructure for external-delegate-options
+    options_structure = Gst.Structure.new_empty("QNNExternalDelegate")
+    options_structure.set_value("backend_type", "htp")
+    elements["tflite"].set_property("external-delegate-options", options_structure)
+
+    # Add elements to the pipeline
+    for element in elements.values():
+        pipeline.add(element)
+
+    # Link elements
+    link_elements(elements)
+
+def main():
+    """Main function to set up and run the GStreamer pipeline."""
+
+    os.environ["XDG_RUNTIME_DIR"] = "/dev/socket/weston"
+    os.environ["WAYLAND_DISPLAY"] = "wayland-1"
+
+    # Initialize GStreamer
+    Gst.init(None)
+    mloop = GLib.MainLoop()
+
+    # Create the pipeline
+    try:
+        pipeline = Gst.Pipeline.new("cam-detect-display-pipeline")
+        if not pipeline:
+            raise Exception(f"Unable to create pipeline")
+        create_pipeline(pipeline)
+    except Exception as e:
+        print(f"{e}, exiting...")
+        exit(-1)
+
+    # Handle Ctrl+C
+    interrupt_watch_id = GLib.unix_signal_add(
+        GLib.PRIORITY_HIGH, signal.SIGINT, handle_interrupt_signal, pipeline, mloop
+    )
+
+    # Wait until error or EOS
+    bus = pipeline.get_bus()
+    bus.add_signal_watch()
+    bus.connect("message", handle_bus_message, mloop)
+
+    # Start playing
+    print("Setting to PLAYING...")
+    pipeline.set_state(Gst.State.PLAYING)
+    mloop.run()
+
+    GLib.source_remove(interrupt_watch_id)
+    bus.remove_signal_watch()
+    bus = None
+
+    print("Setting to NULL...")
+    pipeline.set_state(Gst.State.NULL)
+
+    mloop = None
+    pipeline = None
+    Gst.deinit()
+
+if __name__ == "__main__":
+    sys.exit(main())
