13ba1e0b5ec898431daf6bdb9c2f518c2a31a901
quic_srijaror <quic_srijaror@quicinc.com>
2025-01-09 17:49:37 +0530

    gst-ai-sample-app: Adding Config file support
    Change-Id: Iafac058abd16dd8dfa3965731d33d59eb7430447

--- gst-sample-apps/gst-ai-multistream-batch-inference/main.c
+++ gst-sample-apps/gst-ai-multistream-batch-inference/main.c
@@ -113,7 +113,7 @@
 #define DEFAULT_DISPLAY_WIDTH 1920
 #define DEFAULT_DISPLAY_HEIGHT 1080
 
-#define DEFAULT_CONFIG_FILE "/etc/configs/batch_config.json"
+#define DEFAULT_CONFIG_FILE "/etc/configs/config_multistream-batch-inference.json"
 
 /**
  * Structure for various application specific options
