5a475dc6e4257f1524a9477ba6e96f5a2bf237b4
Jiadong Huang <quic_jiadhuan@quicinc.com>
2025-01-14 11:04:46 +0800

    qtimlaclassification: Add post-processing plugin
    Add post-processing plugin for audio classification
    
    Change-Id: Ia6af76a1fe79ff38c22b253bef674d1d9a66ec20

---
+++ gst-plugin-mlaclassification/modules/ml-aclassification-yamnet.c
@@ -0,0 +1,191 @@
+/*
+* Copyright (c) 2025 Qualcomm Innovation Center, Inc. All rights reserved.
+* SPDX-3-License-Identifier: BSD-3-Clause-Clear
+*/
+
+#include <math.h>
+
+#include <gst/utils/common-utils.h>
+#include <gst/utils/batch-utils.h>
+#include <gst/ml/ml-module-utils.h>
+#include <gst/ml/ml-module-audio-classification.h>
+
+
+// Set the default debug category.
+#define GST_CAT_DEFAULT gst_ml_module_debug
+
+#define GST_ML_SUB_MODULE_CAST(obj) ((GstMLSubModule*)(obj))
+
+#define GST_ML_MODULE_CAPS \
+    "neural-network/tensors, " \
+    "type = (string) { INT8, UINT8, INT32, FLOAT32 }, " \
+    "dimensions = (int) < <1, 521> >"
+
+// Module caps instance
+static GstStaticCaps modulecaps = GST_STATIC_CAPS (GST_ML_MODULE_CAPS);
+
+typedef struct _GstMLSubModule GstMLSubModule;
+
+struct _GstMLSubModule {
+  // Configurated ML capabilities in structure format.
+  GstMLInfo  mlinfo;
+
+  // List of prediction labels.
+  GHashTable *labels;
+  // Confidence threshold value.
+  gdouble    threshold;
+
+};
+
+gpointer
+gst_ml_module_open (void)
+{
+  GstMLSubModule *submodule = NULL;
+
+  submodule = g_slice_new0 (GstMLSubModule);
+  g_return_val_if_fail (submodule != NULL, NULL);
+
+  return (gpointer) submodule;
+}
+
+void
+gst_ml_module_close (gpointer instance)
+{
+  GstMLSubModule *submodule = GST_ML_SUB_MODULE_CAST (instance);
+
+  if (NULL == submodule)
+    return;
+
+  if (submodule->labels != NULL)
+    g_hash_table_destroy (submodule->labels);
+
+  g_slice_free (GstMLSubModule, submodule);
+}
+
+GstCaps *
+gst_ml_module_caps (void)
+{
+  static GstCaps *caps = NULL;
+  static gsize inited = 0;
+
+  if (g_once_init_enter (&inited)) {
+    caps = gst_static_caps_get (&modulecaps);
+    g_once_init_leave (&inited, 1);
+  }
+
+  return caps;
+}
+
+gboolean
+gst_ml_module_configure (gpointer instance, GstStructure * settings)
+{
+  GstMLSubModule *submodule = GST_ML_SUB_MODULE_CAST (instance);
+  GstCaps *caps = NULL, *mlcaps = NULL;
+  const gchar *input = NULL;
+  GValue list = G_VALUE_INIT;
+  gdouble threshold = 0.0;
+  gboolean success = FALSE;
+
+  g_return_val_if_fail (submodule != NULL, FALSE);
+  g_return_val_if_fail (settings != NULL, FALSE);
+
+  if (!(success = gst_structure_has_field (settings, GST_ML_MODULE_OPT_CAPS))) {
+    GST_ERROR ("Settings stucture does not contain configuration caps!");
+    goto cleanup;
+  }
+
+  // Fetch the configuration capabilities.
+  gst_structure_get (settings, GST_ML_MODULE_OPT_CAPS, GST_TYPE_CAPS, &caps, NULL);
+  // Get the set of supported capabilities.
+  mlcaps = gst_ml_module_caps ();
+
+  // Make sure that the configuration capabilities are fixated and supported.
+  if (!(success = gst_caps_is_fixed (caps))) {
+    GST_ERROR ("Configuration caps are not fixated!");
+    goto cleanup;
+  } else if (!(success = gst_caps_can_intersect (caps, mlcaps))) {
+    GST_ERROR ("Configuration caps are not supported!");
+    goto cleanup;
+  }
+
+  if (!(success = gst_ml_info_from_caps (&(submodule->mlinfo), caps))) {
+    GST_ERROR ("Failed to get ML info from confguration caps!");
+    goto cleanup;
+  }
+
+  input = gst_structure_get_string (settings, GST_ML_MODULE_OPT_LABELS);
+
+  // Parse funtion will print error message if it fails, simply goto cleanup.
+  if (!(success = gst_ml_parse_labels (input, &list)))
+    goto cleanup;
+
+  submodule->labels = gst_ml_load_labels (&list);
+
+  // Labels funtion will print error message if it fails, simply goto cleanup.
+  if (!(success = (submodule->labels != NULL)))
+    goto cleanup;
+
+  success = gst_structure_has_field (settings, GST_ML_MODULE_OPT_THRESHOLD);
+  if (!success) {
+    GST_ERROR ("Settings stucture does not contain threshold value!");
+    goto cleanup;
+  }
+
+  gst_structure_get_double (settings, GST_ML_MODULE_OPT_THRESHOLD, &threshold);
+  submodule->threshold = threshold;
+
+cleanup:
+  if (caps != NULL)
+    gst_caps_unref (caps);
+
+  g_value_unset (&list);
+  gst_structure_free (settings);
+
+  return success;
+}
+
+gboolean
+gst_ml_module_process (gpointer instance, GstMLFrame * mlframe, gpointer output)
+{
+  GstMLSubModule *submodule = GST_ML_SUB_MODULE_CAST (instance);
+  GArray *predictions = (GArray *) output;
+  GstMLClassPrediction *prediction = NULL;
+  guint8 *data = NULL;
+  guint idx = 0, n_inferences = 0;
+  gdouble confidence = 0.0;
+
+  g_return_val_if_fail (submodule != NULL, FALSE);
+  g_return_val_if_fail (mlframe != NULL, FALSE);
+  g_return_val_if_fail (predictions != NULL, FALSE);
+
+  prediction = &(g_array_index (predictions, GstMLClassPrediction, 0));
+
+  n_inferences = GST_ML_FRAME_DIM (mlframe, 0, 1);
+  data = GST_ML_FRAME_BLOCK_DATA (mlframe, 0);
+
+  // Fill the prediction table.
+  for (idx = 0; idx < n_inferences; ++idx) {
+    GstMLLabel *label = NULL;
+    GstMLClassEntry entry = { 0 };
+
+    confidence = GFLOAT_PTR_CAST(data) [idx];
+    confidence *= 100;
+
+    // Discard results with confidence below the set threshold.
+    if (confidence < submodule->threshold)
+      continue;
+
+    label = g_hash_table_lookup (submodule->labels, GUINT_TO_POINTER (idx));
+
+    entry.confidence = confidence;
+    entry.name = g_quark_from_string (label ? label->name : "unknown");
+    entry.color = label ? label->color : 0x000000FF;
+
+    prediction->entries = g_array_append_val (prediction->entries, entry);
+  }
+
+  g_array_sort (prediction->entries,
+      (GCompareFunc) gst_ml_class_audio_compare_entries);
+
+  return TRUE;
+}
