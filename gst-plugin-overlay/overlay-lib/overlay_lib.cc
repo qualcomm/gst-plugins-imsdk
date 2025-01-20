9bf344ece68fc69bcbea6f476838fa03312e5193
shrasoma <quic_shrasoma@quicinc.com>
2025-01-20 18:21:16 +0530

    gst-plugin-overlay: Remove unused downstream GBM code
    Change-Id: Iad8f2080786ed1c73d9f4456867e93e08c3830b2

--- gst-plugin-overlay/overlay-lib/overlay_lib.cc
+++ gst-plugin-overlay/overlay-lib/overlay_lib.cc
@@ -52,12 +52,6 @@
 
 #ifdef HAVE_MMM_COLOR_FMT_H
 #include <display/media/mmm_color_fmt.h>
-#else
-#include <media/msm_media_info.h>
-#define MMM_COLOR_FMT_NV12_UBWC COLOR_FMT_NV12_UBWC
-#define MMM_COLOR_FMT_ALIGN MSM_MEDIA_ALIGN
-#define MMM_COLOR_FMT_Y_META_STRIDE VENUS_Y_META_STRIDE
-#define MMM_COLOR_FMT_Y_META_SCANLINES VENUS_Y_META_SCANLINES
 #endif // HAVE_MMM_COLOR_FMT_H
 
 #include "tools.h"
