00dfd1c70128b33992a62b3b963d93f9c80b44a4
Petar G. Georgiev <quic_petarg@quicinc.com>
2025-02-06 13:28:45 +0200

    vsplit, vcomposer: Fix transfer of nested VideoLandmarks meta
    Issue: Not all fields were copied when transfering VideoLandmarks meta
           nested in ROI meta.
    Fix: Properly transfer nested VideoLandmarks meta by copying all fields
    
    Change-Id: Ief97e0c32e455d8fb42bb48cf3237d2782b5104b

--- gst-plugin-vcomposer/videocomposer.c
+++ gst-plugin-vcomposer/videocomposer.c
@@ -329,9 +329,11 @@ gst_buffer_transfer_video_region_of_interest_meta (GstBuffer * buffer,
         kp->y = kp->y * h_scale;
       }
 
-      structure = gst_structure_new ("VideoLandmarks",
-          "keypoints", G_TYPE_ARRAY, keypoints, "links", G_TYPE_ARRAY, links,
-          "confidence", G_TYPE_DOUBLE, confidence, NULL);
+      structure = gst_structure_copy (structure);
+      gst_structure_set (structure, "keypoints", G_TYPE_ARRAY, keypoints,
+          "links", G_TYPE_ARRAY, links, "confidence", G_TYPE_DOUBLE,
+          confidence, NULL);
+
       gst_video_region_of_interest_meta_add_param (newmeta, structure);
     } else if (id == g_quark_from_static_string ("ImageClassification")) {
       structure = gst_structure_copy (structure);
