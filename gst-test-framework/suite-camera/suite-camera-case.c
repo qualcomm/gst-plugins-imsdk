8be2619a0c99fca5a96347891d46bdb7272841a4
Kin Zhao <quic_yunjiez@quicinc.com>
2025-02-24 15:41:49 +0800

    test-framework: update camera pipeline
    Issue: pipleline could not be launched due to GBM/UBWC in caps.
    
    Fix: update camera pipelines.
    
    Change-Id: I1a5cdf0692c496dd99d5866b2902a0b503d1db9d

--- gst-test-framework/suite-camera/suite-camera-case.c
+++ gst-test-framework/suite-camera/suite-camera-case.c
@@ -12,14 +12,14 @@ gint runningtime = 3; // Default running time 3 seconds.
 
 GST_START_TEST (test_stream_NV12_1920x1080_30fps)
 {
-  GstCapsParameters params = { "NV12", 1920, 1080, 30};
+  GstCapsParameters params = { "NV12_Q08C", 1920, 1080, 30};
   camera_pipeline (&params, NULL, NULL, NULL, runningtime);
 }
 GST_END_TEST;
 
 GST_START_TEST (test_streams_NV12_1280x70_30fps_NV12_1920x1080_30fps)
 {
-  GstCapsParameters params1 = { "NV12", 1920, 1080, 30};
+  GstCapsParameters params1 = { "NV12_Q08C", 1920, 1080, 30};
   GstCapsParameters params2 = { "NV12", 1280, 720, 30};
   camera_pipeline (&params1, &params2, NULL, NULL, runningtime);
 }
@@ -27,7 +27,7 @@ GST_END_TEST;
 
 GST_START_TEST (test_streams_NV12_1280x720_30fps_JPEG_1920x1080)
 {
-  GstCapsParameters params = { "NV12", 1280, 720, 30};
+  GstCapsParameters params = { "NV12_Q08C", 1280, 720, 30};
   GstCapsParameters jpegparams = { "JPEG", 1920, 1080, 1};
   camera_pipeline (&params, NULL, NULL, &jpegparams, runningtime);
 }
@@ -35,8 +35,8 @@ GST_END_TEST;
 
 GST_START_TEST (test_streams_NV12_1280x720_30fps_JPEG_1920x1080_RAW_)
 {
-  GstCapsParameters params = { "NV12", 1280, 720, 30};
-  GstCapsParameters rawparams= { "rggb", 4096, 3072, 1};
+  GstCapsParameters params = { "NV12_Q08C", 1280, 720, 30};
+  GstCapsParameters rawparams= { "rggb", 4056, 3040, 1};
   GstCapsParameters jpegparams = { "JPEG", 1280, 720, 1};
   camera_pipeline (&params, NULL, &rawparams, &jpegparams, runningtime);
 }
@@ -44,7 +44,7 @@ GST_END_TEST;
 
 GST_START_TEST (test_streams_NV12_1920x1080_30FPS_NV12_1280x720_60fps_JPEG_1920x1080)
 {
-  GstCapsParameters params1 = { "NV12", 1920, 1080, 30};
+  GstCapsParameters params1 = { "NV12_Q08C", 1920, 1080, 30};
   GstCapsParameters params2 = { "NV12", 1280, 720, 30};
   GstCapsParameters jpegparams = { "JPEG", 1280, 720, 1};
   camera_pipeline (&params1, &params2, NULL, &jpegparams, runningtime);
@@ -53,9 +53,9 @@ GST_END_TEST;
 
 GST_START_TEST (test_streams_NV12_1920x1080_30FPS_NV12_1280x720_60fps_JPEG_1920x1080_RAW)
 {
-  GstCapsParameters params1 = { "NV12", 1920, 1080, 30};
+  GstCapsParameters params1 = { "NV12_Q08C", 1920, 1080, 30};
   GstCapsParameters params2 = { "NV12", 1280, 720, 30};
-  GstCapsParameters rawparams = { "rggb", 4096, 3072, 1};
+  GstCapsParameters rawparams = { "rggb", 4056, 3040, 1};
   GstCapsParameters jpegparams = { "JPEG", 1280, 720, 1};
   camera_pipeline (&params1, &params2, &rawparams, &jpegparams, runningtime);
 }
@@ -63,7 +63,7 @@ GST_END_TEST;
 
 GST_START_TEST (test_streams_NV12_1920x1080_DISPLAY_NV12_1280x720_60fps_ENCODE)
 {
-  GstCapsParameters params1 = { "NV12", 1920, 1080, 30};
+  GstCapsParameters params1 = { "NV12_Q08C", 1920, 1080, 30};
   GstCapsParameters params2 = { "NV12", 1280, 720, 60};
   camera_display_encode_pipeline (&params1, &params2, runningtime);
 }
