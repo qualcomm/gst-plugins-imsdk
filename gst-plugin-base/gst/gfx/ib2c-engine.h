72c9c5d5d6dd7998c9482be7dc34866e38795130
Petar G. Georgiev <c_petarg@qti.qualcomm.com>
2025-05-27 10:48:28 +0300

    base: ib2c: Initial support for multiple textures per surface
    - Rename the GraphicTuple to SurfaceTuple
    - Expand SurfaceTuple to include multiple textures and images
      per surface.
    
    Change-Id: I6a7a317c8b55960b5839ec8d27ec108267a49d36

--- gst-plugin-base/gst/gfx/ib2c-engine.h
+++ gst-plugin-base/gst/gfx/ib2c-engine.h
@@ -26,8 +26,11 @@ typedef std::tuple<GLsizei, GLsizei, GLenum> TextureTuple;
 // Map of <Shader Type, Shader Program>
 typedef std::map<ShaderType, std::shared_ptr<ShaderProgram>> ShaderPrograms;
 
-// Tuple of <GL texture, EGl image, Surface>
-typedef std::tuple<GLuint, EGLImageKHR, Surface> GraphicTuple;
+// Tuple of <GL textures, EGL image>
+typedef std::tuple<GLuint, EGLImageKHR> GraphicTuple;
+
+// Tuple of <List of GL textures and EGL images, Surface>
+typedef std::tuple<std::vector<GraphicTuple>, Surface> SurfaceTuple;
 
 struct ImagePlane {
   uint32_t stride;
@@ -75,9 +78,12 @@ class Engine : public IEngine {
   GLuint GetStageTexture(const Surface& surface, const Objects& objects);
 
 #if defined(ANDROID)
-  GraphicTuple ImportAndroidSurface(const Surface& surface, uint32_t flags);
+  std::vector<GraphicTuple> ImportAndroidSurface(const Surface& surface,
+                                                 uint32_t flags);
 #else // !ANDROID
-  GraphicTuple ImportLinuxSurface(const Surface& surface, uint32_t flags);
+  std::vector<GraphicTuple> ImportLinuxSurface(const Surface& surface,
+                                               uint32_t flags);
+
   ImageParam GetImageParams(const Surface& surface, uint32_t flags);
 #endif // defined(ANDROID)
 
@@ -97,8 +103,8 @@ class Engine : public IEngine {
   /// Map of <ShaderType, ShaderProgram>
   ShaderPrograms                   shaders_;
 
-  /// Map of surface_id and its GL texture, EGL image and Surface.
-  std::map<uint64_t, GraphicTuple> graphics_;
+  /// Map of surface_id and its GL textures, EGL images and Surface.
+  std::map<uint64_t, SurfaceTuple>  surfaces_;
 };
 
 } // namespace ib2c
