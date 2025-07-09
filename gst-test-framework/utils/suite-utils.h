3744782baccf498abe2aca2040da963abe74188e
Bing Li <bli8@qti.qualcomm.com>
2025-07-08 22:31:05 -0700

    Revert "test_framework: add ml case"
    This reverts commit 42f2cc201618ba440af60597cb5a0100f01b13bf.
    
    Reason for revert: <The dependency of this change will be moved.>
    
    Change-Id: Ic8c5d1163d71fbd688c5ee62a18545f64e408fab

--- gst-test-framework/utils/suite-utils.h
+++ gst-test-framework/utils/suite-utils.h
@@ -12,44 +12,8 @@ G_BEGIN_DECLS
 
 #define GST_VIDEO_CODEC_H264           "H.264"
 #define GST_VIDEO_CODEC_H265           "H.265"
-#define TF_TO_STRING(x)                #x
-#define TF_MODELS_PREFIX               "/etc/models/"
-#define TF_LABELS_PREFIX               "/etc/labels/"
-#define TF_MEDIA_PREFIX                "/etc/media/"
-#define TF_FILE_LOCATION(name)         TF_MEDIA_PREFIX TF_TO_STRING(name)
-#define TF_MODEL_LOCATION(name)        TF_MODELS_PREFIX TF_TO_STRING(name)
-#define TF_LABEL_LOCATION(name)        TF_LABELS_PREFIX TF_TO_STRING(name)
 
 typedef struct _GstCapsParameters GstCapsParameters;
-typedef struct _GstMLFrameInfo GstMLFrameInfo;
-typedef struct _GstMLVideoInfo GstMLVideoInfo;
-typedef struct _GstMLModel GstMLModel;
-
-/**
- * GstModelType:
- * @GST_MODEL_TYPE_NONE  : Invalid Model Type.
- * @GST_MODEL_TYPE_SNPE  : SNPE DLC Container.
- * @GST_MODEL_TYPE_TFLITE: TFLITE Container.
- * @GST_MODEL_TYPE_QNN   : QNN Container.
- *
- * Type of Model container for the Runtime.
- */
-typedef enum {
-  GST_MODEL_TYPE_NONE,
-  GST_MODEL_TYPE_SNPE,
-  GST_MODEL_TYPE_TFLITE,
-  GST_MODEL_TYPE_QNN
-} GstModelType;
-
-/**
- * GstDetectionModuleType:
- * @GST_YOLO_TYPE_V8  : Yolov8 Object Detection Model.
- *
- * Type of Yolo Model.
- */
-typedef enum {
-  GST_YOLO_TYPE_V8,
-} GstDetectionModuleType;
 
 struct _GstCapsParameters {
   const gchar *format;
@@ -57,51 +21,6 @@ struct _GstCapsParameters {
   gint        height;
   gint        fps;
 };
-
-struct _GstMLFrameInfo {
-  gint        index;
-  gint        metanum;
-};
-
-struct _GstMLVideoInfo {
-  const gchar    *file;
-  GstMLFrameInfo frameinfo[10];
-};
-
-typedef enum  {
-  GST_ML_DELEGATE_CPU,
-  GST_ML_DELEGATE_GPU,
-  GST_ML_DELEGATE_DSP
-} GstMLDelegate;
-
-struct _GstMLModel {
-  GstModelType   type;
-  gchar          *modelpath;
-  gchar          *labelspath;
-  gboolean       use_constants;
-  gchar          *constants;
-  gint           moduletype;
-  gfloat         threshold;
-  GstMLDelegate  delegate;
-};
-
-/**
- * GstInferenceType:
- * @param GST_OBJECT_DETECTION: Object detection Pipeline.
- * @param GST_CLASSIFICATION: Classification Pipeline.
- * @param GST_POSE_DETECTION: Pose detection Pipeline.
- * @param GST_SEGMENTATION: Segmentation Pipeline.
- * @param GST_PIPELINE_CNT
- *
- * Type of Inference.
- */
-typedef enum {
-  GST_OBJECT_DETECTION,
-  GST_CLASSIFICATION,
-  GST_POSE_DETECTION,
-  GST_SEGMENTATION,
-  GST_PIPELINE_CNT
-} GstInferenceType;
 
 /**
  * gst_mp4_verification:
