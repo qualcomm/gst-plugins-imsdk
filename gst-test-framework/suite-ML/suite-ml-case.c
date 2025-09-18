0a15b08092db298aef8c2f16bda977c79f221562
Kin Zhao <yunjiez@qti.qualcomm.com>
2025-09-18 18:40:07 +0800

    test framework: use new post processing plugin for ML cases
    Change-Id: I4c5c1248c8cdad2bd67d05f12f0ce33ba4eb3a3f

--- gst-test-framework/suite-ML/suite-ml-case.c
+++ gst-test-framework/suite-ML/suite-ml-case.c
@@ -28,23 +28,51 @@ GstMLVideoInfo detection_videoinfo = {
   .frameinfo[9] = {80, 2},
 };
 
+GstMLVideoInfo classification_videoinfo = {
+  .file = TF_FILE_LOCATION (Animals_000_1080p_180s_30FPS.mp4),
+  .frameinfo[0] = {5, 0},
+  .frameinfo[1] = {10, 0},
+  .frameinfo[2] = {15, 0},
+  .frameinfo[3] = {20, 0},
+  .frameinfo[4] = {30, 1},
+  .frameinfo[5] = {35, 1},
+  .frameinfo[6] = {45, 2},
+  .frameinfo[7] = {55, 2},
+  .frameinfo[8] = {65, 2},
+  .frameinfo[9] = {75, 2},
+};
+
 GST_START_TEST (test_ml_video_tflite_detection_yolov8)
 {
   GstMLModelInfo tflite_yolov8 = {
     .inferencetype = GST_ML_OBJECT_DETECTION,
     .type = GST_ML_MODEL_TFLITE,
     .modelpath = TF_MODEL_LOCATION (yolov8_det_quantized.tflite),
-    .labelspath = TF_LABEL_LOCATION (yolov8.labels),
-    .useconstants = TRUE,
-    .constants = "YOLOv8,q-offsets=<30.0,0.0,0.0>,"
-         "q-scales=<3.2181551456451416,0.0037337171379476786,0.0>;",
+    .labelspath = TF_LABEL_LOCATION (yolov8.json),
+    .settings = "{\"confidence\": 70.0}",
     .moduletype = GST_ML_MODULE_YOLO_V8,
     .results = 10,
-    .threshold = 75.0,
     .delegate = GST_ML_DELEGATE_DSP
   };
 
   ml_video_inference_pipeline (&tflite_yolov8, &detection_videoinfo);
+}
+GST_END_TEST;
+
+GST_START_TEST (test_ml_video_tflite_detection_yolox)
+{
+  GstMLModelInfo tflite_yolox = {
+    .inferencetype = GST_ML_OBJECT_DETECTION,
+    .type = GST_ML_MODEL_TFLITE,
+    .modelpath = TF_MODEL_LOCATION (yolox-yolo-x-w8a8.tflite),
+    .labelspath = TF_LABEL_LOCATION (yolov8.json),
+    .settings = "{\"confidence\": 80.0}",
+    .moduletype = GST_ML_MODULE_YOLO_V8,
+    .results = 10,
+    .delegate = GST_ML_DELEGATE_DSP
+  };
+
+  ml_video_inference_pipeline (&tflite_yolox, &detection_videoinfo);
 }
 GST_END_TEST;
 
@@ -54,16 +82,31 @@ GST_START_TEST (test_ml_video_qnn_detection_yolov8)
     .inferencetype = GST_ML_OBJECT_DETECTION,
     .type = GST_ML_MODEL_QNN,
     .modelpath = TF_MODEL_LOCATION (yolov8_det.bin),
-    .labelspath = TF_LABEL_LOCATION (yolov8.labels),
-    .useconstants = FALSE,
-    .constants = NULL,
+    .labelspath = TF_LABEL_LOCATION (yolov8.json),
+    .settings = NULL,
     .moduletype = GST_ML_MODULE_YOLO_V8,
     .results = 10,
-    .threshold = 51.0,
     .delegate = GST_ML_DELEGATE_DSP
   };
 
   ml_video_inference_pipeline (&qnn_yolov8, &detection_videoinfo);
+}
+GST_END_TEST;
+
+GST_START_TEST (test_ml_video_tflite_classification_mobilenet)
+{
+  GstMLModelInfo tflite_mobilenet = {
+    .inferencetype = GST_ML_CLASSIFICATION,
+    .type = GST_ML_MODEL_TFLITE,
+    .modelpath = TF_MODEL_LOCATION (resnext101-resnext101-w8a8.tflite),
+    .labelspath = TF_LABEL_LOCATION (mobilenet_old.json),
+    .settings = "{\"confidence\": 51.0}",
+    .moduletype = GST_ML_MODULE_MOBILENET_SOFTMAX,
+    .results = 10,
+    .delegate = GST_ML_DELEGATE_DSP
+  };
+
+  ml_video_inference_pipeline (&tflite_mobilenet, &classification_videoinfo);
 }
 GST_END_TEST;
 
@@ -96,13 +139,23 @@ ml_suite (GList **tcnames, gint iteration, gint duration)
   // Add test to TCase tflite yolov8.
   tcase_add_loop_test (tc, test_ml_video_tflite_detection_yolov8, start, end);
 
-  tcname = "qnndetection_yolov8";
+  tcname = "tflitedetection_yolox";
+  tc = tcase_create (tcname);
+  *tcnames = g_list_append (*tcnames, (gpointer)tcname);
+  suite_add_tcase (s, tc);
+  tcase_set_timeout (tc, tctimeout);
+  // Add test to TCase tflite yolox.
+  tcase_add_loop_test (tc, test_ml_video_tflite_detection_yolox, start, end);
+
+  tctimeout = runningtime + 35;
+  tcname = "tflite_classification_mobilenet";
   tc = tcase_create (tcname);
   *tcnames = g_list_append (*tcnames, (gpointer)tcname);
   suite_add_tcase (s, tc);
   tcase_set_timeout (tc, tctimeout);
   // Add test to TCase.
-  tcase_add_loop_test (tc, test_ml_video_qnn_detection_yolov8, start, end);
+  tcase_add_loop_test (tc,
+      test_ml_video_tflite_classification_mobilenet, start, end);
 
   return s;
 }
