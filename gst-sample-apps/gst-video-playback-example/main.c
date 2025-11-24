464a4f33f7140b944739f8091b8c37a797f217d2
Bala Sai Kosuri <bkosuri@qti.qualcomm.com>
2025-11-24 14:04:47 +0530

    Include BSD-3-Clause-Clear license in copyright headers
    Change-Id: I3aa94b5b9b5ec746c50d6e9e7e7210f062aede45

--- gst-sample-apps/gst-video-playback-example/main.c
+++ gst-sample-apps/gst-video-playback-example/main.c
@@ -1,7 +1,7 @@
 /*
-* Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
-* SPDX-License-Identifier: BSD-3-Clause-Clear
-*/
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
 
 #include <stdio.h>
 #include <glib-unix.h>
